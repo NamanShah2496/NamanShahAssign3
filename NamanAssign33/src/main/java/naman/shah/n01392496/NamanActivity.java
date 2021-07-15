8ed17546f2d83dc2de08b04ad25507a5ce60f0f5
naman0301 <65423813+naman0301@users.noreply.github.com>
2021-07-15 15:26:03 -0400

    snackbar done and website link attached in snackbar with addition to onback press

--- NamanAssign33/src/main/java/naman/shah/n01392496/NamanActivity.java
+++ NamanAssign33/src/main/java/naman/shah/n01392496/NamanActivity.java
@@ -1,33 +1,26 @@
 package naman.shah.n01392496;
 // Naman Shah, N01392496, RNA
+import android.content.DialogInterface;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import naman.shah.n01392496.ui.main.SectionsPagerAdapter;
-import naman.shah.n01392496.databinding.ActivityMainBinding;
 
 public class NamanActivity extends AppCompatActivity {
-private TextView textView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager viewPager= (ViewPager) findViewById(R.id.Namanview_pager);
+        ViewPager viewPager= findViewById(R.id.Namanview_pager);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         viewPager.setAdapter(sectionsPagerAdapter);
-        textView = findViewById(R.id.section_label);
         TabLayout tabs = findViewById(R.id.Namantabs);
         tabs.setupWithViewPager(viewPager);
 
@@ -50,5 +43,27 @@ private TextView textView;
         });
 
     }
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(false);
+        builder.setTitle("Naman Shah (N01392496)");
+        builder.setMessage(R.string.exit_dialouge);
+        builder.setIcon(R.drawable.alert);
+        builder.setPositiveButton(R.string.exit_yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton(R.string.exit_no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
+    }
 }
--- NamanAssign33/src/main/java/naman/shah/n01392496/ui/main/NamanFragment.java
+++ NamanAssign33/src/main/java/naman/shah/n01392496/ui/main/NamanFragment.java
@@ -1,16 +1,24 @@
 package naman.shah.n01392496.ui.main;
 // Naman Shah, N01392496, RNA
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Calendar;
 
 import naman.shah.n01392496.R;
 
@@ -21,7 +29,10 @@ import naman.shah.n01392496.R;
 public class NamanFragment extends Fragment {
     private static final String TAG = "1";
     private View v;
+    String website="https://www.linkedin.com/feed/";
     private PageViewModel pageViewModel;
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    Intent intent = null;
 
     public static NamanFragment newInstance() {
 
@@ -40,15 +51,11 @@ public class NamanFragment extends Fragment {
         v = inflater.inflate(R.layout.fragment_naman, container, false);
         final TextView textView = v.findViewById(R.id.section_label);
         pageViewModel.getText().observe(getViewLifecycleOwner(),
-                new Observer<String>() {
-                    @Override
-                    public void onChanged(@Nullable String s) {
-                        textView.setText(s);
-                    }
-                });
+                s -> textView.setText(s));
 
 
-        final ImageButton btn = (ImageButton) v.findViewById(R.id.imageButton);
+        final ImageButton btn = v.findViewById(R.id.namanimageButton);
+        final Button button = v.findViewById(R.id.namanbutton);
 
         btn.setOnClickListener(new View.OnClickListener() {
             int count = 0;
@@ -71,7 +78,31 @@ public class NamanFragment extends Fragment {
                  count++;
             }
         });
-    return v;}
+        button.setOnClickListener(v -> {
+            Calendar cal = Calendar.getInstance();
+
+            DatePickerDialog dialog = new DatePickerDialog(getActivity(),
+                    mDateSetListener, cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+            dialog.show();
+        });
+        mDateSetListener = (view, year, month, day) -> {
+
+                    month = month + 1;
+            ConstraintLayout rootlayout = (ConstraintLayout) v.findViewById(R.id.constraintLayout);
+            Snackbar snackbar = Snackbar
+                    .make(rootlayout, +  month + "/" + day + "/" + year, Snackbar.LENGTH_LONG)
+                    .setAction("Click", view1 -> {
+                        intent = new Intent(Intent.ACTION_VIEW,
+                                Uri.parse(website));
+                        startActivity(intent);
+
+                    });
+
+            snackbar.show();
+        };
+    return v;
+    }
 }
 
 
