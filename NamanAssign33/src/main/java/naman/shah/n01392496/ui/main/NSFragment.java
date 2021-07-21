970f3aaf72d1cfd79a5de26280c12c6f5123cdbc
naman0301 <65423813+naman0301@users.noreply.github.com>
2021-07-21 16:27:01 -0400

    Action_call function added for tab 4 and some changes in manifest as well as string file

--- NamanAssign33/src/main/java/naman/shah/n01392496/ui/main/NSFragment.java
+++ NamanAssign33/src/main/java/naman/shah/n01392496/ui/main/NSFragment.java
@@ -1,67 +1,77 @@
 // Naman Shah, N01392496, RNA
 package naman.shah.n01392496.ui.main;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import org.jetbrains.annotations.NotNull;
 import naman.shah.n01392496.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NSFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NSFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+private View v;
+    private static final int REQUEST_PHONE_CALL = 1;
 
     public NSFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NSFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NSFragment newInstance(String param1, String param2) {
-        NSFragment fragment = new NSFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+       }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+         v = inflater.inflate(R.layout.fragment_n_s, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_n_s, container, false);
+        final Button button = v.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertDummyContactWrapper();
+            }
+        });
+return v;
+    }
+    private void insertDummyContactWrapper() {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse(getString(R.string.tel) + getString(R.string.number)));
+            requireActivity().startActivity(callIntent);
+        } else {
+            ActivityCompat.requestPermissions((Activity) requireContext(), new String[]{Manifest.permission.CALL_PHONE},
+                    REQUEST_PHONE_CALL);
+        }
+    }
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions, @NotNull int[] grantResults) {
+        if (requestCode == REQUEST_PHONE_CALL && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), getResources().getString(R.string.allow), Toast.LENGTH_SHORT).show();
+        }
+        else{
+            ConstraintLayout text = (ConstraintLayout) v.findViewById(R.id.constraintLayout);
+            Snackbar snackbar = Snackbar
+                    .make(text, R.string.deny, Snackbar.LENGTH_LONG);
+                    snackbar.show();
+        }
     }
 }
